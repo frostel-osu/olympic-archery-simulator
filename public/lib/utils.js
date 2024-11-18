a40dee4447f1a3ea66888029de6a6fc1d0a78b29
Ellie Frost <frostel@oregonstate.edu>
2024-11-18 06:37:18 -0800

    Implement simulation

---
+++ public/lib/utils.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const TIME_STEP = 0.01; //seconds
+
+const VALUES = {
+  WEIGHT: {
+    DEFAULT: 15,
+    MAX: 60,
+    MIN: 15
+  },
+  HEIGHT: {
+    DEFAULT: 64,
+    MAX: 84,
+    MIN: 48
+  },
+  VERTICAL_ANGLE: {
+    DEFAULT: 0,
+    MAX: 90,
+    MIN: -90
+  }
+};
+
+const bound_value = (KEY, value) => {
+  const { DEFAULT, MAX, MIN } = VALUES[KEY];
+
+  if (value === "") {
+    return DEFAULT;
+  } else if (value < MIN) {
+    return MIN;
+  } else if (MAX < value) {
+    return MAX;
+  } else {
+    return value;
+  }
+};
+
+const in_to_ft = (n, m = n % 12) => `${(n - m) / 12}'${m ? `${m}"` : ""}`;
+
+export {
+  TIME_STEP,
+  VALUES,
+  bound_value,
+  in_to_ft
+};
--- public/main.js
+++ public/main.js
@@ -1,5 +1,8 @@
 "use strict";
 
+import { VALUES, bound_value, in_to_ft } from "./lib/utils.js";
+import { simulate } from "./lib/simulate.js";
+
 const DOMFormInputs = document.querySelector(".js-form-inputs");
 
 const DOMInputDrawWeight = document.querySelector(".js-input-draw-weight");
@@ -9,26 +12,6 @@ const DOMInputVerticalAngle = document.querySelector(".js-input-vertical-angle")
 
 const DOMButtonFire = document.querySelector(".js-button-fire");
 const DOMButtonReset = document.querySelector(".js-button-reset");
-
-const VALUES = {
-  WEIGHT: {
-    DEFAULT: 15,
-    MIN: 15,
-    MAX: 60
-  },
-  HEIGHT: {
-    DEFAULT: 64,
-    MIN: 48,
-    MAX: 84
-  },
-  VERTICAL_ANGLE: {
-    DEFAULT: 0,
-    MIN: -90,
-    MAX: 90
-  }
-}
-
-const in_to_ft = (n, m = n % 12) => `${(n - m) / 12}'${m ? `${m}"` : ""}`;
 
 const reset_inputs = () => {
   DOMInputDrawWeight.value = localStorage.oas_draw_weight;
@@ -45,43 +28,36 @@ const disable_inputs = (bool = true) => {
   }
 };
 
-const bound_input = (KEY, input) => {
-  const { MIN, MAX } = VALUES[KEY];
-
-  if (input < MIN) {
-    return MIN;
-  } else if (MAX < input) {
-    return MAX;
-  } else {
-    return input;
-  }
-};
-
 localStorage.oas_draw_weight = localStorage.oas_draw_weight || VALUES.WEIGHT.DEFAULT;
 localStorage.oas_height = localStorage.oas_height || VALUES.HEIGHT.DEFAULT;
 
 reset_inputs();
 
 DOMInputDrawWeight.addEventListener("change", ({ target: input }) => {
-  input.value = bound_input("WEIGHT", input.value);
+  input.value = bound_value("WEIGHT", input.value);
   localStorage.oas_draw_weight = input.value;
 });
 
 DOMInputHeight.addEventListener("change", ({ target: input }) => {
-  input.value = bound_input("HEIGHT", input.value);
+  input.value = bound_value("HEIGHT", input.value);
   localStorage.oas_height = input.value;
 
   DOMInputHeightText.textContent = in_to_ft(input.value);
 });
 
 DOMInputVerticalAngle.addEventListener("change", ({ target: input }) => {
-  input.value = bound_input("VERTICAL_ANGLE", input.value);
+  input.value = bound_value("VERTICAL_ANGLE", input.value);
 });
 
 DOMButtonFire.addEventListener("click", (event) => {
   event.preventDefault();
 
+  const form = new FormData(DOMFormInputs);
+  const simulation = simulate(form);
+
   disable_inputs();
+
+  console.log([...simulation()]);
 
   setTimeout(() => { disable_inputs(false); }, 3000);
 });
